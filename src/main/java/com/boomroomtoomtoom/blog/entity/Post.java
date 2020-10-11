70ffd8093a5d68e04d5b60f04be64f2627657fdd
Ildar <you@example.com>
2020-10-11 17:38:16 +0300

    Настроил страницу создание category.html

---
+++ src/main/java/com/boomroomtoomtoom/blog/entity/Post.java
@@ -0,0 +1,39 @@
+package com.boomroomtoomtoom.blog.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(name="posts")
+
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Size(min = 3, message = "Название должно состоять не менее чем из 3 символов.")
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_date")
+    private Date createDate;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "modify_date")
+    private Date modifyDate;
+
+    @ManyToOne
+    @JsonManagedReference
+    private Category category;
+}
---
+++ src/main/java/com/boomroomtoomtoom/blog/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.boomroomtoomtoom.blog.repository;
+
+import com.boomroomtoomtoom.blog.entity.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Integer> {
+}
